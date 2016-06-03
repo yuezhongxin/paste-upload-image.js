7aadbfa2ff8d73ca8f67b89408e2e5c8f531898c
yuezhongxin <yuezhongxin@gmail.com>
2016-06-03 14:58:16 +0800

    Add files

---
+++ scripts/paste-upload-image.js
@@ -0,0 +1,90 @@
+﻿(function ($) {
+    var $this;
+    var $ajaxUrl = 'http://upload.cnblogs.com/imageuploader/ProcessPasteUpload';
+    $.fn.pasteUploadImage = function () {
+        $this = $(this);
+        $this.on('paste', function (event) {
+            var filename, image, pasteEvent, text;
+            pasteEvent = event.originalEvent;
+            if (pasteEvent.clipboardData && pasteEvent.clipboardData.items) {
+                image = isImage(pasteEvent);
+                if (image) {
+                    event.preventDefault();
+                    filename = getFilename(pasteEvent) || "image.png";
+                    text = "{{" + filename + "(uploading...)}}";
+                    pasteText(text);
+                    return uploadFile(image.getAsFile(), filename);
+                }
+            }
+        });
+    };
+
+    pasteText = function (text) {
+        var afterSelection, beforeSelection, caretEnd, caretStart, textEnd;
+        caretStart = $this[0].selectionStart;
+        caretEnd = $this[0].selectionEnd;
+        textEnd = $this.val().length;
+        beforeSelection = $this.val().substring(0, caretStart);
+        afterSelection = $this.val().substring(caretEnd, textEnd);
+        $this.val(beforeSelection + text + afterSelection);
+        $this.get(0).setSelectionRange(caretStart + text.length, caretEnd + text.length);
+        return $this.trigger("input");
+    };
+    isImage = function (data) {
+        var i, item;
+        i = 0;
+        while (i < data.clipboardData.items.length) {
+            item = data.clipboardData.items[i];
+            if (item.type.indexOf("image") !== -1) {
+                return item;
+            }
+            i++;
+        }
+        return false;
+    };
+    getFilename = function (e) {
+        var value;
+        if (window.clipboardData && window.clipboardData.getData) {
+            value = window.clipboardData.getData("Text");
+        } else if (e.clipboardData && e.clipboardData.getData) {
+            value = e.clipboardData.getData("text/plain");
+        }
+        value = value.split("\r");
+        return value[0];
+    };
+    uploadFile = function (file, filename) {
+        var formData = new FormData();
+        formData.append('imageFile', file);
+        formData.append("mimeType", file.type);
+        formData.append("host", 'www.cnblogs.com');
+
+        $.ajax({
+            url: $ajaxUrl,
+            data: formData,
+            type: 'post',
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            success: function (data) {
+                if (data.success) {
+                    return insertToTextArea(filename, data.message);
+                }
+                return replaceLoadingTest(filename);
+            },
+            error: function (xOptions, textStatus) {
+                replaceLoadingTest(filename);
+                console.log(xOptions.responseText);
+            }
+        });
+    };
+    insertToTextArea = function (filename, url) {
+        return $this.val(function (index, val) {
+            return val.replace("{{" + filename + "(uploading...)}}", "![" + filename + "](" + url + ")" + "\n");
+        });
+    };
+    replaceLoadingTest = function (filename) {
+        return $this.val(function (index, val) {
+            return val.replace("{{" + filename + "(uploading...)}}", filename + "\n");
+        });
+    };
+})(jQuery);
---
+++ tinymce/plugins/pasteUpload/plugin.js
@@ -0,0 +1,100 @@
+/**
+ * plugin.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 1999-2015 Ephox Corp. All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*global tinymce:true */
+
+(function () {
+    var host = "www.cnblogs.com";
+    var ajaxUrl = 'http://upload.cnblogs.com/imageuploader/ProcessPasteUpload';
+    var loadingImage = '<img id="loadingImg" src="http://static.cnblogs.com/images/loading.gif" alt="" />';
+
+    tinymce.create('tinymce.plugins.PasteUploadPlugin', {
+        init: function (ed, url) {
+            ed.on("Paste", (function (e) {
+                var image, pasteEvent, text;
+                pasteEvent = e;
+                if (pasteEvent.clipboardData && pasteEvent.clipboardData.items) {
+                    image = isImage(pasteEvent);
+                    if (image) {
+                        e.preventDefault();
+                        ed.execCommand('mceInsertContent', false, loadingImage);
+                        return uploadFile(image.getAsFile(), getFilename(pasteEvent));
+                    }
+                }
+            }));
+
+            function isImage(data) {
+                var i, item;
+                i = 0;
+                while (i < data.clipboardData.items.length) {
+                    item = data.clipboardData.items[i];
+                    if (item.type.indexOf("image") !== -1) {
+                        return item;
+                    }
+                    i++;
+                }
+                return false;
+            };
+            function uploadFile(file, filename) {
+                var formData = new FormData();
+                formData.append('imageFile', file);
+                formData.append("mimeType", file.type);
+                formData.append("host", host);
+
+                $.ajax({
+                    url: ajaxUrl,
+                    data: formData,
+                    type: 'post',
+                    processData: false,
+                    contentType: false,
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.success) {
+                            insertIntoTinymce(data.message);
+                        } else {
+                            replaceLoading(filename);
+                        }
+                    },
+                    error: function (xOptions, textStatus) {
+                        replaceLoading(filename);
+                        console.log(xOptions.responseText);
+                    }
+                });
+            };
+            function insertIntoTinymce(url) {
+                var content = ed.getContent();
+                content = content.replace(loadingImage, '<img src="' + url + '">');
+                ed.setContent(content);
+                ed.selection.select(ed.getBody(), true);
+                ed.selection.collapse(false);
+            };
+            function replaceLoading(filename) {
+                var content = ed.getContent();
+                content = content.replace(loadingImage, filename);
+                ed.setContent(content);
+                ed.selection.select(ed.getBody(), true);
+                ed.selection.collapse(false);
+            };
+            function getFilename(e) {
+                var value;
+                if (window.clipboardData && window.clipboardData.getData) {
+                    value = window.clipboardData.getData("Text");
+                } else if (e.clipboardData && e.clipboardData.getData) {
+                    value = e.clipboardData.getData("text/plain");
+                }
+                value = value.split("\r");
+                return value[0];
+            };
+        },
+    });
+
+    // Register plugin
+    tinymce.PluginManager.add("pasteUpload", tinymce.plugins.PasteUploadPlugin);
+})();
